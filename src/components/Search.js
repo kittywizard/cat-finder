45c4fc34e3d0e19235a42d1db826246a3e478c0b
marleigh <volcana@gmail.com>
2022-02-05 12:25:55 -0800

    Search state change working!

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,23 +2,51 @@ import React from "react";
 
 export default function Search() {
 
-    const [location, setLocation] = React.useState("Kirkland, WA");
+    const [location, setLocation] = React.useState(
+        {locationName: "",
+        locationEntered:false                             
+    });
     
-    function handleClick(e) {
-        e.preventDefault();
+    function handleChange(e) {
 
         const {name, value} = e.target;
+        setLocation(prevState => {
+            return {
+                ...prevState,
+                [name]: value
+            }
+        });
+    }
 
-        console.log(e.target);
+    function handleClick(e) {
+        e.preventDefault();
+        //when button is clicked, display the location on screen
+        setLocation(prevState => {
+            return {
+                ...prevState,
+                locationEntered: !prevState.locationEntered
+            }
+        })
+        return;
     }
 
     return (
         <section className="search">
-            <div className="current-location"><strong>Current Location: </strong> {location}</div>
-            <form onSubmit={handleClick} id="form">
+            <div className="current-location">
+                <strong>Current Location: </strong> 
+                {location.locationEntered && location.locationName}
+            </div>
+            <form 
+                onSubmit={handleClick} 
+                id="form">
                 <i className="fas fa-map-marker-alt location-marker"></i>
-                <input type="text" placeholder="City, State"/>
-                <button type="submit" id="location-submit" onClick={(e) => handleClick(e)}>Go</button>
+                <input type="text" 
+                        placeholder="City, State"
+                        onChange={handleChange}
+                        name="locationName"
+                        value={location.locationName}/>
+                <button type="submit" 
+                        id="location-submit">Go</button>
             </form>
         </section>
     )

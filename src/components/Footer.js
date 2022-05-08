2a125cb12ee71bd2911a62cc05fb9b0dbaf5b5ae
marleigh <volcana@gmail.com>
2022-05-08 12:24:28 -0700

    copy changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,9 +2,6 @@ export default function Footer() {
     return(
         <footer className="footer">
             <p>
-                Copy Courtesy of <a href="http://www.catipsum.com/index.php" className="footer-link">Cat Ipsum</a>
-            </p>
-            <p>
                 Adopt a cat from your local shelter! :)
             </p>
         </footer>

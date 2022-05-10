832ab7b87257d0f3d7f364ebbcd09eea11dd54e6
marleigh <volcana@gmail.com>
2022-05-10 15:21:03 -0700

    clean up favorite component and making a card

--- src/components/FavoriteCard.js
+++ src/components/FavoriteCard.js
@@ -1,11 +1,18 @@
 import Divider from "./Divider";
 
-export default function FavoriteCard() {
+export default function FavoriteCard({cat}) {
     return (
-        <div>
-            card for favorites
-
-            <Divider />
-        </div>
+        <div className="modal-catGroup">
+                <h4 className="modal-catName">{cat.name}</h4>
+                    <div className="modal-catHeader">
+                        <img src={cat.image} alt={cat.name} className="modal-image"/>
+                            <p className="modal-catInfo">
+                                <strong>Breed/Color:</strong> {cat.breed} <br />
+                                <strong>Age: </strong>{cat.age} <br />
+                                <strong>Location: </strong> {cat.hometown}
+                            </p>
+                    </div>
+                    <Divider />
+            </div>
     )
 }

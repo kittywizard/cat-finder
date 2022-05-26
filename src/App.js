0954b7089cf2a2dab8f7f199893ce69dfcca3256
marleigh <volcana@gmail.com>
2022-05-26 12:37:06 -0700

    functional, if not fancy sidebar!

--- src/App.js
+++ src/App.js
@@ -10,13 +10,21 @@ import { useState } from "react";
 export default function App2() {
 
     const [displaySidebar, setDisplaySidebar] = useState(false);
+
+    function showSidebar() {
+        //hide the arrow when not in use?
+       setDisplaySidebar(prevState => !prevState)
+    }
     
     return (
         <div className="root-container">
             <Header /> 
             {
-                displaySidebar &&
-                <Sidebar />
+                displaySidebar ?
+                <Sidebar showSidebar={showSidebar}/> :
+                <div onClick={showSidebar}>
+                    <i className="fas fa-solid fa-arrow-right sidebar-arrow"></i>
+                </div>
             }
             <Routes>
                 <Route path="/" element={<Cats/>}/>
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -3,11 +3,12 @@ import Filter from "./Filter";
 import { useContext } from "react";
 import {Context} from "../Context";
 
-export default function Sidebar() {
+export default function Sidebar(props) {
     const {catFilterData} = useContext(Context);
 
     return (
         <section className="sidebar">
+            <i className="fas fa-solid fa-arrow-left sidebar-close" onClick={props.showSidebar}></i>
              <Filter 
                 catFilterData={catFilterData}
             />
